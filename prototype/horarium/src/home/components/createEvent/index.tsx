c746173ce4e0033a0f245dd8184edab9ccf03893
EdwardBrodskiy <brodskiedward@gmail.com>
2021-03-31 19:06:28 +0100

    added states to form

--- prototype/horarium/src/home/components/createEvent/index.tsx
+++ prototype/horarium/src/home/components/createEvent/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
-  Box,
   Button,
   FormControl,
   FormLabel,
@@ -15,7 +14,7 @@ import {
   Select,
   Textarea,
 } from '@chakra-ui/react'
-import { CalendarEventType, Course, PrefillEventData } from '../../../types'
+import { CalendarEvent, CalendarEventType, Course, PrefillEventData } from '../../../types'
 import store from 'store'
 
 type Props = {
@@ -25,6 +24,24 @@ type Props = {
 }
 
 export const CreateEventModal = ({ onClose, isOpen, prefilledData }: Props) => {
+  const initialState = {
+    title: '',
+    description: '',
+    location: '',
+    startTime: '',
+    duration: '',
+    type: prefilledData.type?.name,
+  }
+  const [{ title, description, location, startTime, duration, type }, setFormData] = useState(
+    initialState,
+  )
+  const clearState = () => {
+    setFormData({ ...initialState })
+  }
+  const onChange: React.ChangeEventHandler<any> = (event) => {
+    const { name, value } = event.target
+    setFormData((prevState) => ({ ...prevState, [name]: value }))
+  }
   const location_options = store.get('locations').map((item: string, index: number) => (
     <option key={index} value={item}>
       {item}
@@ -47,37 +64,70 @@ export const CreateEventModal = ({ onClose, isOpen, prefilledData }: Props) => {
         <ModalBody>
           <FormControl>
             <FormLabel>Title</FormLabel>
-            <Input placeholder={`${prefilledData.type?.name} name`} />
+            <Input
+              name='title'
+              placeholder={`${prefilledData.type?.name} name`}
+              value={title}
+              onChange={onChange}
+            />
           </FormControl>
 
           <FormControl>
             <FormLabel>Description</FormLabel>
-            <Textarea placeholder='Some text here' />
+            <Textarea
+              name='description'
+              placeholder='Some text here'
+              value={description}
+              onChange={onChange}
+            />
           </FormControl>
 
           <FormControl>
             <FormLabel>Location</FormLabel>
-            <Select placeholder='Select option'>{location_options}</Select>
+            <Select
+              name='location'
+              placeholder='Select option'
+              value={location}
+              onChange={onChange}
+            >
+              {location_options}
+            </Select>
           </FormControl>
 
           <FormControl>
             <FormLabel>Start Time</FormLabel>
-            <Input type='datetime-local' />
+            <Input name='startTime' type='datetime-local' value={startTime} onChange={onChange} />
           </FormControl>
 
           <FormControl>
             <FormLabel>Duration</FormLabel>
-            <Input type='time' />
+            <Input name='duration' type='time' value={duration} onChange={onChange} />
           </FormControl>
 
           <FormControl>
             <FormLabel>Event Type</FormLabel>
-            <Select defaultValue={prefilledData.type?.name}>{eventType_options}</Select>
+            <Select
+              name='type'
+              defaultValue={prefilledData.type?.name}
+              placeholder='Select option'
+              value={type}
+              onChange={onChange}
+            >
+              {eventType_options}
+            </Select>
           </FormControl>
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme='blue' mr={3} onClick={onClose}>
+          <Button
+            colorScheme='blue'
+            mr={3}
+            onClick={() => {
+              console.log(`${title} ${description} ${location} ${startTime} ${duration} ${type}`)
+              clearState()
+              onClose()
+            }}
+          >
             Create
           </Button>
           <Button variant='ghost' onClick={onClose}>
@@ -87,4 +137,10 @@ export const CreateEventModal = ({ onClose, isOpen, prefilledData }: Props) => {
       </ModalContent>
     </Modal>
   )
+}
+
+const save_event = (course_index: number, event: CalendarEvent) => {
+  const courses = store.get('course')
+  courses[course_index].push(event)
+  store.set('courses', courses)
 }
