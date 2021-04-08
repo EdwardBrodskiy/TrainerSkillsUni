cbcbc2eb37555b0053566025876b74b410305c3c
EdwardBrodskiy <brodskiedward@gmail.com>
2021-04-08 17:02:46 +0100

    refactor

--- prototype/horarium/src/components/header/index.tsx
+++ prototype/horarium/src/components/header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Flex, useColorMode, Spacer } from '@chakra-ui/react'
+import { Box, Flex, useColorMode, Image, HStack } from '@chakra-ui/react'
 import { Link, useRouteMatch } from 'react-router-dom'
 import { DarkModeToggle } from '../DarkMode'
 import { NavItem } from './NavItem'
@@ -7,14 +7,13 @@ import { MatchParams } from '../../types'
 import logo from '../../images/logo.png'
 import logoDark from '../../images/logoDark.png'
 import { Searchbar } from '../../home/components/searchbar'
-import { generateKeyPair } from 'crypto'
 
 export const Header = () => {
   const match = useRouteMatch<MatchParams>()
 
   const { colorMode } = useColorMode()
   const bgColor = { light: 'gray.200', dark: 'gray.700' }
-  let pageLogo = (colorMode === 'light') ? logo : logoDark;
+  let pageLogo = colorMode === 'light' ? logo : logoDark
 
   return (
     <Box h='4rem'>
@@ -23,46 +22,28 @@ export const Header = () => {
         p={4}
         h='4rem'
         bg={bgColor[colorMode]}
-        pos={'fixed'}
+        pos='fixed'
         left='0'
         right='0'
         top='0'
         borderBottomWidth='1px'
         width='full'
       >
-        
-        <Flex align='center' w='100%' h='100%'>
+        <Flex align='center' justifyContent='space-between' w='100%' h='100%'>
           <Box>
-            <Link to='/'><img alt='logo' src={pageLogo} style={{maxHeight:'50px'}}/></Link>
+            <Link to='/home'>
+              <Image alt='logo' src={pageLogo} style={{ maxHeight: '50px' }} />
+            </Link>
           </Box>
-          <Spacer />
-          <Box justify='space-evenly' maxWidth='180px'> 
-            <NavItem>
-              <Link to='/'>Home</Link>
-            </NavItem>
-          </Box>
-          <Box margin='0 10px'>
-            <Searchbar/>
-          </Box>
-          <Box>
-            <DarkModeToggle />
-          </Box>
-        </Flex>
 
-        {/* <Flex w='100%' h='100%'>
-          <Flex align='center'>
-            <Link to='/'><img alt='logo' src={pageLogo} style={{maxHeight:'50px'}}/></Link>
-          </Flex>
-          <Flex justify='space-evenly' maxWidth='180px'>
+          <HStack spacing={4}>
             <NavItem>
-              <Link to='/'>Home</Link>
+              <Link to='/home'>Home</Link>
             </NavItem>
-          </Flex>
-          <Spacer />
-          <Flex w='60%' align='left'>
+            <Searchbar />
             <DarkModeToggle />
-          </Flex>
-        </Flex> */}
+          </HStack>
+        </Flex>
       </Box>
     </Box>
   )
--- prototype/horarium/src/home/components/searchbar/index.tsx
+++ prototype/horarium/src/home/components/searchbar/index.tsx
@@ -1,43 +1,22 @@
 import React from 'react'
-import {
-    Button,
-    Input,
-    Drawer,
-    ColorMode,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    useDisclosure,
-  } from "@chakra-ui/react"
-import { IconButton, useColorMode } from '@chakra-ui/react'
+import { Drawer, DrawerBody, DrawerOverlay, DrawerContent, useDisclosure } from '@chakra-ui/react'
+import { IconButton } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
 
 export const Searchbar = () => {
-  const { colorMode } = useColorMode()
-  const bgColor = { light: 'gray.200', dark: 'gray.700' }
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = React.useRef()
   return (
     <>
-    <IconButton
-      aria-label='change color mode'
-      onClick={onOpen}
-      icon={<Search2Icon/>}
-      isRound={true}
-    />
-    <Drawer
-        isOpen={isOpen}
-        placement="left"
-        size="xs"
-        onClose={onClose}
-      >
+      <IconButton
+        aria-label='change color mode'
+        onClick={onOpen}
+        icon={<Search2Icon />}
+        isRound={true}
+      />
+      <Drawer isOpen={isOpen} placement='left' size='xs' onClose={onClose}>
         <DrawerOverlay>
           <DrawerContent>
-            <DrawerBody>
-            </DrawerBody>
+            <DrawerBody></DrawerBody>
           </DrawerContent>
         </DrawerOverlay>
       </Drawer>
