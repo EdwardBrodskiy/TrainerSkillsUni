9c1446f41ccd145e8b21b4607806474dee108039
ec19343 <t.toma@se19.qmul.ac.uk>
2021-04-08 18:17:17 +0300

    search

--- prototype/horarium/src/home/components/searchbar/index.tsx
+++ prototype/horarium/src/home/components/searchbar/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {
     Button,
+    Box,
     Input,
+    InputGroup,
+    InputLeftElement,
     Drawer,
+    Text,
+    Divider,
     ColorMode,
     DrawerBody,
     DrawerFooter,
@@ -13,30 +18,64 @@ import {
     useDisclosure,
   } from "@chakra-ui/react"
 import { IconButton, useColorMode } from '@chakra-ui/react'
+import { Modules } from '../../../types'
 import { Search2Icon } from '@chakra-ui/icons'
+import store from 'store'
+import { useState } from 'react'
 
 export const Searchbar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
   const { colorMode } = useColorMode()
   const bgColor = { light: 'gray.200', dark: 'gray.700' }
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = React.useRef()
+  const location_results = store.get('locations').filter((item: string, index: number)=> {
+    if(searchTerm == "") return '';
+    else if( item.toLowerCase().includes(searchTerm.toLowerCase())){
+      return item;
+    }
+  })
+  function display(result_type : string,result: []) {
+    if(result.length>0){
+      return(
+        <Box>
+          <Text>{result_type}</Text>
+          <Divider />
+          {result.map((item: string, index: number) => (
+            <li key={index} value={item}>
+              {item}
+            </li>
+            ))}
+        </Box>
+      )
+    }
+  }
+  console.log(location_results);
   return (
     <>
-    <IconButton
-      aria-label='change color mode'
-      onClick={onOpen}
-      icon={<Search2Icon/>}
-      isRound={true}
-    />
-    <Drawer
+        <IconButton
+          aria-label='change color mode'
+          onClick={onOpen}
+          icon={<Search2Icon/>}
+          isRound={true}
+        />  
+        <Drawer
         isOpen={isOpen}
-        placement="left"
-        size="xs"
+        placement="right"
         onClose={onClose}
       >
         <DrawerOverlay>
-          <DrawerContent>
+          <DrawerContent> 
+            <DrawerHeader>
+              <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<Search2Icon color="gray.300" />}
+              />
+              <Input type="text" placeholder="Search.." onChange={event => {setSearchTerm(event.target.value)}}/>
+              </InputGroup>
+            </DrawerHeader>
             <DrawerBody>
+              {display("Locations",location_results)}
             </DrawerBody>
           </DrawerContent>
         </DrawerOverlay>
@@ -44,3 +83,16 @@ export const Searchbar = () => {
     </>
   )
 }
+
+{/* <Text color="gray.500">Locations</Text>
+                <Divider />
+              {store.get('locations').filter((item: string, index: number)=> {
+              if(searchTerm == "") return '';
+              else if( item.toLowerCase().includes(searchTerm.toLowerCase())){
+                return item;
+              }
+            }).map((item: string, index: number) => (
+              <li key={index} value={item}>
+                {item}
+                </li>
+              ))} */}
